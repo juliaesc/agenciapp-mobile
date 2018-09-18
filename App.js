13707595d7c290cd8c6210df31aff73e4e3ad5e4
Julia Escobio <juliaescobio@gmail.com>
2018-09-17 22:58:43 -0300

    Autenticación con Okta y token.
    Se suben los cambios para que se logueé contra Okta y devuelva un token.

--- App.js
+++ App.js
@@ -1,21 +1,160 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Buffer } from 'buffer';
+import { UIManager, LayoutAnimation, Alert } from 'react-native';
+import { authorize, refresh, revoke } from 'react-native-app-auth';
+import { Page, Button, ButtonContainer, Form, Heading } from './components';
 
-export default class App extends React.Component {
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+
+const scopes = ['openid', 'profile', 'email', 'offline_access'];
+
+type State = {
+  hasLoggedInOnce: boolean,
+  accessToken: ?string,
+  accessTokenExpirationDate: ?string,
+  refreshToken: ?string,
+  idToken: ?string
+};
+
+const config = {
+  issuer: 'https://dev-534787.oktapreview.com/oauth2/default',
+  clientId: '0oag9lpj33Kme6OYb0h7',
+  redirectUrl: 'com.oktapreview.dev-534787:/callback',
+  additionalParameters: {},
+  scopes: ['openid', 'profile', 'email', 'offline_access']
+};
+
+export default class App extends Component<{}, State> {
+  state = {
+    hasLoggedInOnce: false,
+    accessToken: '',
+    accessTokenExpirationDate: '',
+    refreshToken: '',
+    idToken: '',
+    beers: []
+  };
+
+  animateState(nextState: $Shape<State>, delay: number = 0) {
+    setTimeout(() => {
+      this.setState(() => {
+        LayoutAnimation.easeInEaseOut();
+        return nextState;
+      });
+    }, delay);
+  }
+
+authorize = async () => {
+  try {
+    const authState = await authorize(config);
+    this.animateState(
+      {
+        hasLoggedInOnce: true,
+        accessToken: authState.accessToken,
+        accessTokenExpirationDate: authState.accessTokenExpirationDate,
+        refreshToken: authState.refreshToken,
+        idToken: authState.idToken
+      },
+      500
+    );
+  } catch (error) {
+    Alert.alert('Failed to log in', error.message);
+  }
+};
+
+  refresh = async () => {
+    try {
+      const authState = await refresh(config, {
+        refreshToken: this.state.refreshToken
+      });
+
+      this.animateState({
+        accessToken: authState.accessToken || this.state.accessToken,
+        accessTokenExpirationDate:
+        authState.accessTokenExpirationDate || this.state.accessTokenExpirationDate,
+        refreshToken: authState.refreshToken || this.state.refreshToken
+      });
+    } catch (error) {
+      Alert.alert('Failed to refresh token', error.message);
+    }
+  };
+
+  revoke = async () => {
+    try {
+      await revoke(config, {
+        tokenToRevoke: this.state.accessToken,
+        sendClientId: true
+      });
+      this.animateState({
+        accessToken: '',
+        accessTokenExpirationDate: '',
+        refreshToken: ''
+      });
+    } catch (error) {
+      Alert.alert('Failed to revoke token', error.message);
+    }
+  };
+
+  fetchGoodBeers = async () => {
+    if (this.state.beers.length) {
+      // reset to id token if beers is already populated
+      this.animateState({ beers: [] });
+    } else {
+      fetch('http://192.168.0.229:8080/good-beers', {
+        headers: {
+          Authorization: `Bearer ${this.state.accessToken}`
+        }
+      }).then(response => response.json())
+        .then(data => {
+          this.animateState({ beers: data });
+        })
+        .catch(error => console.error(error));
+    }
+  };
+
   render() {
+    const { state } = this;
+    if (state.idToken) {
+      const jwtBody = state.idToken.split('.')[1];
+      const base64 = jwtBody.replace('-', '+').replace('_', '/');
+      const decodedJwt = Buffer.from(base64, 'base64');
+      state.idTokenJSON = JSON.parse(decodedJwt);
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Page>
+        {state.accessToken ? (
+          <Form>
+            <Form.Label>accessToken</Form.Label>
+            <Form.Value>{state.accessToken}</Form.Value>
+            <Form.Label>{state.beers.length ? 'Good Beers' : 'ID Token'}</Form.Label>
+            <Form.Value>
+              {JSON.stringify(state.beers.length ? state.beers : state.idTokenJSON)}
+            </Form.Value>
+            <Form.Label>accessTokenExpirationDate</Form.Label>
+            <Form.Value>{state.accessTokenExpirationDate}</Form.Value>
+            <Form.Label>refreshToken</Form.Label>
+            <Form.Value>{state.refreshToken}</Form.Value>
+          </Form>
+        ) : (
+          <Heading>{state.hasLoggedInOnce ? 'Goodbye.' : 'Hello, stranger.'}</Heading>
+        )}
+
+        <ButtonContainer>
+          {!state.accessToken && (
+            <Button onPress={this.authorize} text="Authorize" color="#017CC0" />)}
+          {!!state.refreshToken &&
+            <Button onPress={this.refresh} text="Refresh" color="#24C2CB" /> }
+          {!!state.accessToken &&
+            <Button onPress={this.revoke} text="Revoke" color="#EF525B" /> }
+          {!!state.accessToken &&
+            <Button
+              onPress={this.fetchGoodBeers}
+              text={!this.state.beers.length ? 'Good Beers' : 'ID Token'}
+              color="#008000"
+            />
+          }
+        </ButtonContainer>
+      </Page>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
